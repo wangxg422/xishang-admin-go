7b1bacb21a68ef5a3b2e45c80362df532892180c
wangxg <wangxg422@outlook.com>
2023-06-26 23:58:57 +0800

    changling

--- config/constant.go
+++ config/constant.go
@@ -3,4 +3,13 @@ package config
 const (
 	EnvConfigFile     = "CONFIG_FILE"
 	DefaultConfigFile = "config.yaml"
+
+	MenuIsFrame    = 0
+	MenuIsNotFrame = 1
+
+	MenuIsCache    = 0
+	MenuIsNotCache = 1
+
+	MenuVisible   = "0"
+	MenuNoVisible = "1"
 )
